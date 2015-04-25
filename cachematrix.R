b08c8d54fab44104d01b02aa3868ff68a492eb1c
Vikram Rathnam <vrathnam@gmail.com>
2015-04-25 11:43:02 -0500

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below two functions makeCacheMatrix and cacheSolve will cache the inverse of a matrix.
 
-## Write a short comment describing this function
+
+## The function "makeCacheMatrix" creates a special matrix object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        ## Initialize a variable for inverse function
+        m <- NULL
+        
+        ## Set the matrix
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        ## Get the matrix
+        get <- function() x                                     ## Return the matrix
+        
+        ## Set inverse of the matrix
+        setinverse <- function(inverse) m <<- inverse 
+        
+        ## Get inverse of the matrix
+        getinverse <- function() m                              ## Return the inverse
+        
+        ## Returning a list of above actions
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## The function "cacheSolve" computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getinverse()                             ## Return the matrix that is inverse of 'x'
+        
+        ## Getting the cace data
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        
+        data <- x$get()                                 ## Subseting for the matrix
+        
+        m <- solve(data, ...)                           ## Calculating Inverse
+        
+        x$setinverse(m)                                 ## Set the Inverse for 'x'
+        
+        m                                               ## Return a matrix that is the inverse of 'x'
+        
+        
 }
